dddcc1b747174f073305cf6c710fcc072e7daf3c
Matt Busche <mrbusche@gmail.com>
2020-10-26 20:39:38 -0500

    Updates for list formats

--- src/main/kotlin/com/webpage/checker/CheckerService.kt
+++ src/main/kotlin/com/webpage/checker/CheckerService.kt
@@ -16,9 +16,9 @@ class CheckerService {
             val priceDropItems: MutableList<String> = mutableListOf()
             for (elem in elements) {
                 val element = elem.attr("class", SECTION_LIST_ITEM_CLASS)
-                if (element.childNode(1).childNodeSize() == 6) {
+                if (element.childNode(1).childNodeSize() == 6 || element.childNode(1).childNodeSize() == 7) {
                     try {
-                        val priceDrop = element.childNode(1).childNode(4).childNode(1).childNode(0).toString().replace("Price dropped ", "").replace("%", "").trim().toInt()
+                        val priceDrop = element.childNode(1).childNode(4).childNode(1).childNode(0).toString().replace("Price dropped ", "").replace("%", "").replace("$", "").trim().toInt()
                         val newPrice = element.childNode(1).childNode(2).childNode(1).childNode(1).childNode(0).childNode(0).toString().replace("$", "").toDouble()
                         val itemTitle = (element.childNode(0).childNode(0).childNode(0).childNode(0) as TextNode).wholeText
                         if (percent && priceDrop >= drop) {

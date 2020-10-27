00b1fa90fc235032510321ac927a9acda821a0af
Matt Busche <mrbusche@gmail.com>
2020-10-26 20:54:33 -0500

    clean up verbiage

--- src/test/kotlin/CheckerApplicationTests.kt
+++ src/test/kotlin/CheckerApplicationTests.kt
@@ -3,20 +3,23 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 internal class CheckerApplicationTests {
+    // variables for testing by maximum price
     private val priceDropUrl = "https://www.amazon.com/gp/registry/wishlist/153OV2P85MJD6?sort=universal-price"
+    private val maxPrice = 5
+    
+    // variables for testing by price drop percentage
     private val percentDropUrl = "https://www.amazon.com/gp/registry/wishlist/2YYV6XNS7GF9U?sort=universal-price"
     private val priceDropPercent = 60
-    private val maxPrice = 5
 
     @Test
-    fun `Test New Items By Price`() {
+    fun `Test find items by Maximum Price`() {
         val priceDrops = CheckerService.retrieveByPriceDrop(priceDropUrl, false, maxPrice)
         println(priceDrops.toString())
         assertEquals(0, priceDrops.size)
     }
 
     @Test
-    fun `Test New Items By Price Drop Percentage`() {
+    fun `Test find items by Price Drop Percentage`() {
         val priceDrops = CheckerService.retrieveByPriceDrop(percentDropUrl, true, priceDropPercent)
         println(priceDrops.toString())
         assertEquals(0, priceDrops.size)

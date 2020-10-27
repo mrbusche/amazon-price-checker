543219eaf2163fd5fd8bd891775f5c781d240cc6
Matt Busche <mrbusche@gmail.com>
2020-10-26 20:48:02 -0500

    Update CheckerApplicationTests.kt

--- src/test/kotlin/CheckerApplicationTests.kt
+++ src/test/kotlin/CheckerApplicationTests.kt
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test
 internal class CheckerApplicationTests {
     private val priceDropUrl = "https://www.amazon.com/gp/registry/wishlist/153OV2P85MJD6?sort=universal-price"
     private val percentDropUrl = "https://www.amazon.com/gp/registry/wishlist/2YYV6XNS7GF9U?sort=universal-price"
-    private val priceDropPercent = 50
+    private val priceDropPercent = 60
     private val maxPrice = 5
 
     @Test

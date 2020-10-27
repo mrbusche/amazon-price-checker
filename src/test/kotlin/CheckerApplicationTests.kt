9ad3724506132fb454d4fda7a3138506dbd7ac5b
Matt Busche <mrbusche@gmail.com>
2020-10-26 20:50:26 -0500

    Updates for different list formats

--- src/test/kotlin/CheckerApplicationTests.kt
+++ src/test/kotlin/CheckerApplicationTests.kt
@@ -3,20 +3,21 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 internal class CheckerApplicationTests {
-    private val url = "https://www.amazon.com/gp/registry/wishlist/153OV2P85MJD6?sort=universal-price"
+    private val priceDropUrl = "https://www.amazon.com/gp/registry/wishlist/153OV2P85MJD6?sort=universal-price"
+    private val percentDropUrl = "https://www.amazon.com/gp/registry/wishlist/2YYV6XNS7GF9U?sort=universal-price"
     private val priceDropPercent = 60
     private val maxPrice = 5
 
     @Test
-    fun `Test New Items By Price Drop Percentage`() {
-        val priceDrops = CheckerService.retrieveByPriceDrop(url, true, priceDropPercent)
+    fun `Test New Items By Price`() {
+        val priceDrops = CheckerService.retrieveByPriceDrop(priceDropUrl, false, maxPrice)
         println(priceDrops.toString())
         assertEquals(0, priceDrops.size)
     }
 
     @Test
-    fun `Test New Items By Price`() {
-        val priceDrops = CheckerService.retrieveByPriceDrop(url, false, maxPrice)
+    fun `Test New Items By Price Drop Percentage`() {
+        val priceDrops = CheckerService.retrieveByPriceDrop(percentDropUrl, true, priceDropPercent)
         println(priceDrops.toString())
         assertEquals(0, priceDrops.size)
     }

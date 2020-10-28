4831eeab5ac6a3028d2bca16e83801f2c2a283ca
Matt Busche <mrbusche@gmail.com>
2020-10-27 21:16:28 -0500

    catch errors better (#8)

--- src/main/kotlin/com/webpage/checker/CheckerService.kt
+++ src/main/kotlin/com/webpage/checker/CheckerService.kt
@@ -16,22 +16,24 @@ class CheckerService {
             val priceDropItems: MutableList<String> = mutableListOf()
             for (elem in elements) {
                 val element = elem.attr("class", SECTION_LIST_ITEM_CLASS)
-                if (element.childNode(1).childNodeSize() == 6 || element.childNode(1).childNodeSize() == 7) {
-                    try {
-                        val priceDrop = element.childNode(1).childNode(4).childNode(1).childNode(0).toString().replace("Price dropped ", "").replace("%", "").replace("$", "").trim().toInt()
-                        val newPrice = element.childNode(1).childNode(2).childNode(1).childNode(1).childNode(0).childNode(0).toString().replace("$", "").toDouble()
-                        val itemTitle = (element.childNode(0).childNode(0).childNode(0).childNode(0) as TextNode).wholeText
-                        if (percent && priceDrop >= drop) {
-                            priceDropItems.add(itemTitle)
+                try {
+                    if (element.childNode(1).childNodeSize() == 6 || element.childNode(1).childNodeSize() == 7) {
+                        try {
+                            val priceDrop = element.childNode(1).childNode(4).childNode(1).childNode(0).toString().replace("Price dropped ", "").replace("%", "").replace("$", "").trim().toInt()
+                            val newPrice = element.childNode(1).childNode(2).childNode(1).childNode(1).childNode(0).childNode(0).toString().replace("$", "").toDouble()
+                            val itemTitle = (element.childNode(0).childNode(0).childNode(0).childNode(0) as TextNode).wholeText
+                            if (percent && priceDrop >= drop) {
+                                priceDropItems.add(itemTitle)
+                            }
+                            if (!percent && drop >= newPrice) {
+                                priceDropItems.add(itemTitle)
+                            }
+                        } catch (nfe: NumberFormatException) {
+                            println(nfe.message)
                         }
-                        if (!percent && drop >= newPrice) {
-                            priceDropItems.add(itemTitle)
-                        }
-                    } catch (nfe: NumberFormatException) {
-                        println(nfe.message)
-                    } catch (ioe: IndexOutOfBoundsException) {
-                        println(ioe.message)
                     }
+                } catch (ioe: IndexOutOfBoundsException) {
+                    println(ioe.message)
                 }
             }
             return priceDropItems

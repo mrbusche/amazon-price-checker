7464a35465466466b366b185fb71c566355a284d
Matt Busche <mrbusche@gmail.com>
2021-02-23 07:50:01 -0600

    Update CheckerApplicationTests.kt

--- src/test/kotlin/CheckerApplicationTests.kt
+++ src/test/kotlin/CheckerApplicationTests.kt
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test
 internal class CheckerApplicationTests {
     // variables for testing by maximum price
     private val priceDropUrl = "https://www.amazon.com/gp/registry/wishlist/153OV2P85MJD6?sort=universal-price"
+    private val priceDropUrl2 = "https://www.amazon.com/gp/registry/wishlist/NF8ZG9H5H6PQ?sort=universal-price"
     private val maxPrice = 5
     
     // variables for testing by price drop percentage
@@ -14,6 +15,13 @@ internal class CheckerApplicationTests {
     @Test
     fun `Test find items by Maximum Price`() {
         val priceDrops = CheckerService.retrieveByPriceDrop(priceDropUrl, false, maxPrice)
+        priceDrops.forEach { println(it) }
+        assertEquals(0, priceDrops.size)
+    }
+    
+    @Test
+    fun `Test find items by Maximum Price 2`() {
+        val priceDrops = CheckerService.retrieveByPriceDrop(priceDropUrl2, false, maxPrice)
         priceDrops.forEach { println(it) }
         assertEquals(0, priceDrops.size)
     }
